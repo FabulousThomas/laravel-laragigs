9dbda28382b55d3c7cd52d7e7bbdf75e68bd940c
Mba Thomas <angelfab631@gmail.com>
2023-02-13 00:19:44 +0100

    Update ListingController.php

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,22 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     // === GET AND SHOW ALL LISTINGS
     public function index()
     {
-        return view('index', [
-            'listings' => Listing::all(),
+        return view('listings.index', [
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
     // === SHOW SINGLE LISTING
     public function show(Listing $listing)
     {
-        return view('listing', [
+        return view('listings.show', [
             'listing' => $listing,
         ]);
+    }
+
+    // === SHOW CREATE FORM
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // === STORE LISTING DATA
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'website' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Listing Created Successfully!');
     }
 }
