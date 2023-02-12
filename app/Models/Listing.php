89c48d448fd8bb04291a082a4a2b41f0af04bb52
Mba Thomas <angelfab631@gmail.com>
2023-02-13 00:19:40 +0100

    Update Listing.php

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    // protected $fillable = ['description', 'title', 'company', 'email', 'website', 'tags', 'location'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        if ($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        if ($filters['search'] ?? false) {
+            $query->where('tags', 'like', '%' . request('search') . '%')
+                ->orwhere('description', 'like', '%' . request('search') . '%')
+                ->orwhere('title', 'like', '%' . request('search') . '%');
+        }
+    }
 }
