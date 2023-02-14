fade42e346d137161b5a6014e1eae08535f8e3de
Mba Thomas <angelfab631@gmail.com>
2023-02-14 23:19:05 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // === RELATIONSHITP WITH LISTINGS
+    public function listings()
+    {
+        return $this->hasMany(Listing::class, 'user_id');
+    }
 }
